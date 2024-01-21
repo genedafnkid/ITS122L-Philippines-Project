ea1246cdbc18e91694bc9dcbd1e8fbba8225305f
ammpineda <pinedaandreamaym@gmail.com>
2024-01-22 07:34:07 +0800

    -created laravel project as api backend -built migrations for creating tables and database via mysql -created models for each table

---
+++ back-end/database/migrations/2024_01_21_232148_create_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('role');
+            $table->string('email');
+            $table->string('user_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('password');
+            $table->string('display_image')->nullable();
+            $table->dateTime('registered_date');
+            $table->dateTime('last_login_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
---
+++ back-end/database/migrations/2024_01_21_232914_create_adoption_applications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('adoption_applications', function (Blueprint $table) {
+            $table->id();
+            $table->longText('comment');
+            $table->string('status');
+            $table->dateTime('meet_date')->nullable();
+            $table->dateTime('submission_date');
+            $table->dateTime('review_date')->nullable();
+            $table->dateTime('results_date')->nullable();
+            $table->dateTime('modified_date')->nullable();
+            $table->unsignedBigInteger('applicant_id');
+            $table->unsignedBigInteger('pet_id');
+            $table->timestamps();
+
+            $table->foreign('applicant_id')->references('id')->on('adopters');
+            $table->foreign('pet_id')->references('id')->on('pets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adoption_applications');
+    }
+};
